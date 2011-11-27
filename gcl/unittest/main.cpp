a021b1b9ed85c7f07a1f4cb1e842320bb8cb54f8
fcoulombe <fcoulombe@silentfalls.org>
2011-11-27 11:12:34 -0800

    franky adds path util static class with dirname function plyus unittest

--- gcl/unittest/main.cpp
+++ gcl/unittest/main.cpp
@@ -45,6 +45,7 @@
 #include "AssertTest.h"
 #include "ExceptionTest.h"
 #include "HashTest.h"
+#include "PathTest.h"
 #include "Matrix44Test.h"
 #include "Point2Test.h"
 #include "Point3Test.h"
@@ -63,6 +64,7 @@ int main(int /*argc*/, char **argv)
       AssertTest::Test();
       SafeCastTest::Test();
       HashTest::Test();
+      PathTest::Test();
 
       Point2Test::Test();
       Point3Test::Test();
