504d53905929b9f002ee48cb919f9b17f7a34d76
fcoulombe <fcoulombe@silentfalls.org>
2012-02-21 14:38:01 -0600

    franky boiler plates some code

--- gcl/Hash.cpp
+++ gcl/Hash.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2011 by Francois Coulombe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "gcl/Hash.h"
 #include <cstring>
 #include <stdint.h>
@@ -9,30 +31,30 @@ using namespace GCL;
 
 uint32_t Hash::DJB(const char* str, size_t len)
 {
-  uint32_t hash = 5381;
-  if (len == 0)
-    len = strlen(str);
-  for(size_t i = 0; i < len; str++, i++)
-    {
-      hash = ((hash << 5) + hash) + (*str);
-    }
+	uint32_t hash = 5381;
+	if (len == 0)
+		len = strlen(str);
+	for(size_t i = 0; i < len; str++, i++)
+	{
+		hash = ((hash << 5) + hash) + (*str);
+	}
 
-  return hash;
+	return hash;
 
 }
 
 Hash::MD5Hash Hash::MD5(const char* str, size_t len)
 {
-  if (len == 0)
-    len = strlen(str);
+	if (len == 0)
+		len = strlen(str);
 
-  md5_state_s state;
-  md5_init(&state);
-  md5_append(&state, (const uint8_t*)str, len);
+	md5_state_s state;
+	md5_init(&state);
+	md5_append(&state, (const uint8_t*)str, len);
 
-  MD5Hash hash;
-  md5_finish(&state, hash.digest);
-  return hash;
+	MD5Hash hash;
+	md5_finish(&state, hash.digest);
+	return hash;
 }
 
 
