7dee6bf66fe033eb0684b88986ba928138891016
fcoulombe <fcoulombe@silentfalls.org>
2012-02-21 14:38:22 -0600

    franky fixes identation

--- gcl/md5.h
+++ gcl/md5.h
@@ -1,24 +1,24 @@
 /*
-  Copyright (C) 1999, 2002 Aladdin Enterprises.  All rights reserved.
+	  Copyright (C) 1999, 2002 Aladdin Enterprises.  All rights reserved.
 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
+	  This software is provided 'as-is', without any express or implied
+	  warranty.  In no event will the authors be held liable for any damages
+	  arising from the use of this software.
 
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
+	  Permission is granted to anyone to use this software for any purpose,
+	  including commercial applications, and to alter it and redistribute it
+	  freely, subject to the following restrictions:
 
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
+	  1. The origin of this software must not be misrepresented; you must not
+		 claim that you wrote the original software. If you use this software
+		 in a product, an acknowledgment in the product documentation would be
+		 appreciated but is not required.
+	  2. Altered source versions must be plainly marked as such, and must not be
+		 misrepresented as being the original software.
+	  3. This notice may not be removed or altered from any source distribution.
 
-  L. Peter Deutsch
-  ghost@aladdin.com
+	  L. Peter Deutsch
+	  ghost@aladdin.com
 
  */
 /* $Id: md5.h,v 1.4 2002/04/13 19:20:28 lpd Exp $ */
@@ -65,9 +65,9 @@ typedef unsigned int md5_word_t; /* 32-bit word */
 
 /* Define the state of the MD5 Algorithm. */
 typedef struct md5_state_s {
-    md5_word_t count[2];	/* message length in bits, lsw first */
-    md5_word_t abcd[4];		/* digest buffer */
-    md5_byte_t buf[64];		/* accumulate block */
+	md5_word_t count[2];	/* message length in bits, lsw first */
+	md5_word_t abcd[4];		/* digest buffer */
+	md5_byte_t buf[64];		/* accumulate block */
 } md5_state_t;
 
 #ifdef __cplusplus
